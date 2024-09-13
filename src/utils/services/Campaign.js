fcba95c709a6dfefac05585af09328e94c6815da
alireza afshar <alireza@hauskey.com>
2024-09-13 14:32:19 -0400

    Updates

--- src/utils/services/Campaign.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-export const getCampaignsList = () => {
-    return () => axios.get('/api/campaigns')
-}
-
-export const getMetricsInfo = (cid, num) => {
-    return () => axios.get(`/api/campaigns/${cid}?number=${num}`)
-}
--- src/utils/services/index.js
+++
@@ -1,1 +0,0 @@
-export { getCampaignsList } from './Campaign'
